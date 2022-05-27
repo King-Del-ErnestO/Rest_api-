7edee4354afeaf42ac0278d8eb0ecd1e64e08478
King-Del-ErnestO <delernestoa@gmail.com>
2022-05-27 18:08:36 +0100

    inital commit

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from bson import ObjectId
 import jwt
 from datetime import datetime, timedelta
 from functools import wraps
+import os
 
 
 app = Flask(__name__)
@@ -212,6 +213,7 @@ def by_id(item_id):
 
 
 
+port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(debug=True, host='0.0.0.0', port=port)
 
